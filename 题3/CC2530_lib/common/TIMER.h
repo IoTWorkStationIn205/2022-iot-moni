2ca853f791d853bcdc9735a0b88fd0931e289c3f
paopaozhi <254237702@qq.com>
2022-08-11 11:00:58 +0800

    删除文件

--- 题3/CC2530_lib/common/TIMER.h
+++
@@ -1,4 +0,0 @@
-void Timer4_Init(void);
-void Timer4_On(void);
-void Timer4_Off(void);
-uint8 GetSendDataFlag(void);
--- 题3/CC2530_lib/mylib/UART_SEND_RECV.h
+++
@@ -1,4 +0,0 @@
-
-void MyByteCopy(uint8 *dst, int dststart, uint8 *src, int srcstart, int len);
-uint16 RecvUartData(uint8 *uRxData, uint16 uRxlen);
-uint16 RecvUart1Data(uint8 *uRxData, uint16 uRxlen);
